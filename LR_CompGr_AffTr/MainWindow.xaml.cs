49786e4750e7266dc3e7a15577ab47167eeb477d
scenacheros <scenacheros@yandex.ru>
2020-12-13 14:56:11 +0100

    Добавьте файлы проекта.

---
+++ LR_CompGr_AffTr/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.IO;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using System.Drawing;
+using System.Reflection;
+
+namespace LR_CompGr_AffTr
+{
+    
+    public partial class MainWindow : Window
+    {
+        Shape shape;
+        double w;
+        double h;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+            Loaded += DrawAxis;
+        }
+       
+        public void DrawAxis(object sender, EventArgs e)
+        {
+            w = DrawingArea.RenderSize.Width; //получаем координату х 
+            h = DrawingArea.RenderSize.Height; //и координату у центра холста
+
+            shape = new Shape(w, h);
+            shape.BuildAxis();
+            DrawingArea.Children.Add(shape.Ox);
+            DrawingArea.Children.Add(shape.Oy);
+            DrawingArea.Children.Add(shape.OxArrow);
+            DrawingArea.Children.Add(shape.OyArrow);
+        }
+      
+        private void Draw(double[,] v)
+        {
+            for (int i = 0; i < v.GetLength(0)-1 ; i++)
+            {
+                Line line = null;
+
+
+                line = new Line()
+                {
+                    X1 = v[i, 0],
+                    Y1 = v[i, 1],
+                    X2 = v[i + 1, 0],
+                    Y2 = v[i + 1, 1]
+
+                };
+               Random rnd = new Random();
+         
+                SolidColorBrush scb = new SolidColorBrush(System.Windows.Media.Color.FromArgb(255, (byte)rnd.Next(0, 255), (byte)rnd.Next(0, 255), (byte)rnd.Next(0, 255))); 
+
+                line.Stroke = scb;
+                Canvas.SetLeft(line, shape.W / 2);
+                Canvas.SetTop(line, shape.H / 2);
+
+                bool isShown = false;
+
+                if (!isShown)
+                {
+                    isShown = true;
+                    var dbAnim = new DoubleAnimation();
+                    dbAnim.From = 1.0;
+                    dbAnim.To = 0.0;
+                    dbAnim.Duration = new Duration(TimeSpan.FromSeconds(5));
+
+                    DrawingArea.Children.Add(line);
+
+                    line.BeginAnimation(Line.OpacityProperty, dbAnim);
+                }
+            }
+        }
+
+        private void BasicFButton_Click(object sender, RoutedEventArgs e)
+        {
+          Draw(shape.Vs);
+        }
+
+        private void RotateFButton_Click(object sender, RoutedEventArgs e)
+        {
+          Draw(shape.GetVertices(shape.Rotate(46)));
+          Draw(shape.GetVertices(shape.Rotate(112)));
+        }
+
+        private void ReflectFButton_Click(object sender, RoutedEventArgs e)
+        {
+          Draw(shape.Reflect());
+        }
+
+        private void ScaleFButton_Click(object sender, RoutedEventArgs e)
+        {
+           Draw(shape.GetScaledVertices(2.45, 1.21));
+        }
+        private void MoveFButton_Click(object sender, RoutedEventArgs e)
+        {
+            Draw(shape.GetTranslatedVertices(23, 89));
+            Draw(shape.GetTranslatedVertices(-123, 45));
+            Draw(shape.GetTranslatedVertices(123, -89));
+        }
+
+    }
+}
---
+++ LR_CompGr_AffTr/Shape.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+
+namespace LR_CompGr_AffTr
+{
+    class Shape
+    {
+
+       double w;
+       double h;
+
+        //координаты вершин исходной фигуры
+        double[,] vs = { { -60, -125, 1 }, { 70, -125, 1 }, { -100, 120, 1 }, {95, 55, 1}, { -60, -125, 1 } };
+
+        public Polygon OxArrow = null, OyArrow = null, shp=null;
+        public Line Ox = null, Oy = null;
+
+        public double[,] Vs
+        {
+            get => vs;
+            private set => vs = value;
+        }
+
+        public double W { get => w; set => w = value; }
+        public double H { get => h; set => h = value; }
+
+        public Shape()
+        { }
+
+            public Shape(double width, double height)
+        {
+            W = width;
+            H = height;
+        }
+     
+        // задаём формулу поворота 
+        public double[,] Rotate(int angle)
+        {
+            double rad = angle * Math.PI / 180;
+
+            return new[,]
+            {
+                {Math.Cos(rad), Math.Sin(rad), 0},
+                {-Math.Sin(rad), Math.Cos(rad), 0},
+                {0,0,1}
+            };
+        }
+        // задаём формулу отражения
+        public double[,] Reflect()
+        {
+            var r = new double[Vs.GetLength(0), Vs.GetLength(1)];
+
+            for (int i = 0; i < Vs.GetLength(0); i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    r[i, j] = Convert.ToInt32(Vs[i, j] * (-1));
+                }
+            }
+            return r;
+        }
+        // получаем вершины для поворота и отражения путём применения формул поворота и отражения к исходному массиву вершин
+        public double[,] GetVertices(double[,] b)
+        {
+            var r = new double[Vs.GetLength(0), b.GetLength(1)];
+
+            for (int i = 0; i < Vs.GetLength(0); i++)
+            {
+                for (int j = 0; j < b.GetLength(1); j++)
+                {
+                    for (int k = 0; k < b.GetLength(0); k++)
+                    {
+                        r[i, j] += Convert.ToInt32(Vs[i, k] * b[k, j]);
+                    }
+                }
+            }
+            return r;
+        }
+        // получаем вершины для перемещения 
+        public double[,] GetTranslatedVertices(double lambda, double mu)
+        {
+            var r = new double[Vs.GetLength(0), Vs.GetLength(1)];
+
+            for (int i = 0; i < Vs.GetLength(0); i++)
+            {
+                 r[i, 0] = Convert.ToInt32(Vs[i, 0] + lambda);
+                 r[i, 1] = Convert.ToInt32(Vs[i, 1] + mu);
+                 r[i, 2] = Convert.ToInt32(Vs[i, 2] + 0);
+            }
+            return r;           
+        }
+        // получаем масштабированные вершины 
+        public double[,] GetScaledVertices(double alpha, double delta)
+        {
+            PointCollection rv = new PointCollection();
+
+            var r = new double[Vs.GetLength(0), Vs.GetLength(1)];
+
+            for (int i = 0; i < Vs.GetLength(0); i++)
+            {
+                r[i, 0] = Convert.ToInt32(Vs[i, 0] * alpha);
+                r[i, 1] = Convert.ToInt32(Vs[i, 1] * delta);
+                r[i, 2] = Convert.ToInt32(Vs[i, 2] + 0);
+            }
+            return r;
+        }
+        // формируем оси координат со стрелками
+        public void BuildAxis()
+        {
+            // ось абсцисс
+            Ox = new Line()
+            {
+                StrokeThickness = 1,
+                Stroke = Brushes.Gray,
+                X1 = 5,
+                X2 =W-5,
+                Y1 = H / 2,
+                Y2 = H / 2
+            };
+            // ось ординат
+            Oy = new Line()
+            {
+                StrokeThickness = 1,
+                Stroke = Brushes.Gray,
+                X1 = W / 2,
+                X2 = W / 2,
+                Y1 = 5,
+                Y2 = H-5
+            };
+            // стрелка оси абсцисс
+            OxArrow = new Polygon()
+            {
+                StrokeThickness = 1,
+                Stroke = Brushes.Gray,
+                Fill = Brushes.Gray,
+                Points = { new Point(W-5, H / 2), new Point(W - 15, H / 2 - 5), new Point(W - 15, H / 2 + 5) }
+            };
+            // стрелка оси ординат
+            OyArrow = new Polygon()
+            {
+                StrokeThickness = 1,
+                Stroke = Brushes.Gray,
+                Fill = Brushes.Gray,
+                Points = { new Point(W / 2, 5), new Point(W / 2 - 4, 15), new Point(W / 2 + 4, 15) }
+            };
+
+        }
+    }
+}
